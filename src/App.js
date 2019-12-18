ae6dc636664d2088b942b377cc828d52a19c68cc
Clara Nahornyj <c.nahornyj@hotmail.fr>
2019-12-18 21:00:24 +0100

    rectification des conneries ne me frappez pas bisous

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import reducer from './reducers/reducer';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import Navbar from './components/Navbar'
 
 const store = createStore(reducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
