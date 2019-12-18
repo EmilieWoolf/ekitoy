b7da49c4aa995fd32eeab8d12ab760fba04bbf56
Signupz <ludovic.mazet@gmail.com>
2019-12-18 14:24:15 +0100

    Commit avant pull dev et merge dev

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
+
 function App() {
   return (
     <div className="App">
+    <div class="snowflakes" aria-hidden="true">
+    <div class="snowflake">
+    ❅
     </div>
+    <div class="snowflake">
+    ❅
+    </div>
+    <div class="snowflake">
+    ❆
+    </div>
+    <div class="snowflake">
+    ❄
+    </div>
+    <div class="snowflake">
+    ❅
+    </div>
+    <div class="snowflake">
+    ❆
+    </div>
+    <div class="snowflake">
+    ❄
+    </div>
+    <div class="snowflake">
+    ❅
+    </div>
+    <div class="snowflake">
+    ❆
+    </div>
+    <div class="snowflake">
+    ❄
+    </div>
+    </div>
+  </div>
   );
 }
 
