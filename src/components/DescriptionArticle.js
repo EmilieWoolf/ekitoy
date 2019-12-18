332f1c1dfd7345e10dd3f1ffb2ce7bb212774004
Clara Nahornyj <c.nahornyj@hotmail.fr>
2019-12-18 14:23:55 +0100

    Ajout des différentes informations

--- src/components/DescriptionArticle.js
+++ src/components/DescriptionArticle.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import { FaLongArrowAltLeft } from 'react-icons/fa';
 import barbie from '../img/barbie.jpeg';
 import barbie_1 from '../img/barbie_1.jpeg';
 import barbie_2 from '../img/barbie_2.jpeg';
@@ -21,24 +22,30 @@ export default class DescriptionArticle extends Component {
                         <img src={barbie_2} alt="photo3" />
                     </div>
                 </Carousel>
-                <div style={{textAlign: "left", marginLeft:"4%"}}>
+                <div className="container" style={{justifyContent:"space-between"}}>
+                    <button style={{fontSize:"40px"}}><FaLongArrowAltLeft/></button>
+                    <button style={{fontSize:"40px"}}>❤️</button>
+                </div>
+                <div style={{textAlign: "left", marginLeft:"4%"}}>                    
                     <h1>Barbie ever flex</h1>
-                    <h2>Bon état</h2>
+                        <div className="container" style={{display: "flex", flexDirection:"row"}}>
+                            <h2>Bon état •</h2>
+                            <h2 style={{color:"pink", marginLeft:"2%"}}>Barbie</h2>
+                        </div>                    
                     <h3>12,00€</h3>
                 </div>
                 <div className="conteneur">
                     <div>
-                    <button style={{backgroundColor:"pink", borderRadius:"10px", margin:"10px", padding:"10px", fontSize:"20px"}}>Envoyer un message</button>
+                    <button style={{backgroundColor:"pink", borderRadius:"10px", width:"80%", margin:"10px", padding:"10px", fontSize:"20px"}}>Envoyer un message</button>
                     </div>
                     <div>
-                    <button style={{backgroundColor:"#ccc", borderRadius:"10px", width:"201px", padding:"10px", fontSize:"20px"}}>Acheter</button>
+                    <button style={{backgroundColor:"#ccc", borderRadius:"10px", width:"80%", padding:"10px", fontSize:"20px"}}>Acheter</button>
                     </div>
                 </div>
                 <div style={{textAlign:"left", marginLeft:"4%"}}>
-                    <h2>PRESENTATION DE L'ARTICLE</h2>
+                    <h2>Présentation de l'article</h2>
                     <p>Sublime barbie avec son pantalon imitation cuir argenté et ses différentes perruques de rock star</p>
                 </div>
-                
             </div>
             );
         }
