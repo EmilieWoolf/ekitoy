a5a89e079840183b2512663c0fb53c3f0f36a57c
Morgane Mottey <morgane.mottey@gmail.com>
2019-12-19 20:10:27 +0100

    ajout dossier

--- src/back/app.js
+++ src/back/app.js
@@ -8,6 +8,7 @@ const port = 5000 ;
 
 app.use(express.json());
 
+app.use(express.static('public'));
 
 const app = express();
 app.use(require('cors')())
--- src/back/init.sql
+++ src/back/init.sql
@@ -81,6 +81,18 @@ CREATE TABLE IF NOT EXISTS `ekitoy`.`toy` (
 ENGINE = InnoDB;
 
 
+ALTER TABLE `ekitoy`.`toy` CHANGE `categories` `categories` VARCHAR(255) NULL;
+ALTER TABLE `ekitoy`.`toy` CHANGE `keyword` `keyword` VARCHAR(255) NULL;
+ALTER TABLE `ekitoy`.`toy` CHANGE `etat` `etat` VARCHAR(255) NULL;
+
+
+
+INSERT INTO `ekitoy`.`toy` ( `name`, `description`,`price`,`pictures`) VALUES 
+('Camion Barbie', 'Petit bus rose, fonctionne sans piles . Jamais ouvert', '18', 'img1.png'),
+('Lego Architecture', 'Tour à construire , modele New York . Jamais ouvert', '18', 'img2.png'),
+('Lara Croft Figurine', 'Figure de collection . Neuf','15', 'img3.png'),
+('Game Boy Color', 'Modele transaprent vintage . Parfait Etat', '8', 'img4.png'),
+('Barbie Agent Secret', 'Poupée mannequin pour jeunes filles. Dans son emballage , non ouvert. Envoi soigné & rapide!', '5', 'img5.png');
 -- -----------------------------------------------------
 -- Table `ekitoy`.`categories`
 -- -----------------------------------------------------
