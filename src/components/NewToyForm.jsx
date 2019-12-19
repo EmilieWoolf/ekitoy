4a97bb4020b8cf91030645fd574e463f3e468608
Clara Nahornyj <c.nahornyj@hotmail.fr>
2019-12-19 17:53:35 +0100

    Modiiiiifs

--- src/components/NewToyForm.jsx
+++ src/components/NewToyForm.jsx
@@ -8,11 +8,11 @@ const NewToyForm = () => {
             <button style={{display:"block", margin:"auto", border:"1px solid #ccc", borderRadius:"50px", padding:"5px"}}>+ Ajoutez des photos</button>
             <form style={{marginTop:"15px", marginBottom:"15px"}}>
                     <div style={{textAlign:"left", marginLeft:"35px", fontSize:"1.5rem"}}><label for="titre">Titre</label>{'  '}</div>
-                    <div style={{marginBottom:"10px"}}><input type="text" name="titre" required style={{backgroundColor:"#e8e3ce", color:"black", width:"300px", height:"50px"}}/></div>
+                    <div style={{marginBottom:"10px"}}><input type="text" name="titre" required style={{backgroundColor:"#e2dfbc", color:"black", width:"300px", height:"50px", borderRadius:"3px"}}/></div>
                     <div style={{textAlign:"left", marginLeft:"35px", fontSize:"1.5rem"}}><label for="description">Description</label>{'  '}</div>
-                    <div style={{marginBottom:"10px"}}><input type="textarea" name="description" required style={{backgroundColor:"#e8e3ce", color:"black", width:"300px"}}/></div>
+                    <div style={{marginBottom:"10px"}}><input type="textarea" name="description" required style={{backgroundColor:"#e2dfbc", color:"black", width:"300px", borderRadius:"3px"}}/></div>
                     <div style={{textAlign:"left", marginLeft:"35px", fontSize:"1.5rem"}}><label for="price" required>Prix en euros</label>{'  '}</div>
-                    <div style={{marginBottom:"10px"}}><input type="number" name="price" style={{backgroundColor:"#e8e3ce", color:"black", width:"300px"}}/></div>     
+                    <div style={{marginBottom:"10px"}}><input type="number" name="price" style={{backgroundColor:"#e2dfbc", color:"black", width:"300px", borderRadius:"3px"}}/></div>     
             </form>
             <Button type="submit" style={{backgroundColor:"#70c5f4", color:"black", marginLeft:"180px"}}>Vendre</Button>
         </div>
